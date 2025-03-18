c0b8a2338473e610c9cdfd230031cb0e63f11367
hitu-1995 <hiteshwankhede11@gmail.com>
2025-03-18 09:31:15 +0530

    first commit by master

---
+++ Demo.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void main()
+{
+    printf("First Line by master \n");
+}
--- Test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main()
-{
-  printf("First Line \n");
-  printf("Second Line \n");
-  printf("Third Line \n");
-  printf("Fourth Line \n");
-}
